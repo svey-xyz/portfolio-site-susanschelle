3d58164f7e62c5934f5356c55d0f6a21975de788
svey <x@svey.xyz>
2023-11-03 18:44:58 +0000

    Clone of Susan's site

---
+++ schelle_menu.js
@@ -0,0 +1,3 @@
+<!--
+document.write ('<dl><dt><a href="gallerywork.html">gallery work</a></dt><dt><a href="videowork.html">video work</a></dt><dt><a href="publicworks.html">public work</a></dt><dt><a href="collaborative.html">collaborative</a></dt><dt><a href="archive.html">archive</a></dt><dt><a href="contact.html">contact</a></dt><dt><a href="bio.html">bio</a></dt><dt><a href="cv.html">cv</a></dt></dl>')
+-->
